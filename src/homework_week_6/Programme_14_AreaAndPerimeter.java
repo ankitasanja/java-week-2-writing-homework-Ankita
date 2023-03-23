a425d663a00229993c3c216f1221f3e9e3069381
ankitasanja <ankitasanja11@gmail.com>
2023-03-23 14:30:42 +0000

    My homework week 6

---
+++ src/homework_week_6/Programme_14_AreaAndPerimeter.java
@@ -0,0 +1,34 @@
+package homework_week_6;
+
+import java.util.Scanner;
+
+/**
+ * Write a Java program to print the area and perimeter of a rectangle.
+ * Test Data:
+ * Width = 5.5 Height = 8.5
+ * Expected Output
+ * Area is 5.6 * 8.5 = 47.60
+ * Perimeter is 2 * (5.6 + 8.5) = 28.20
+ */
+public class Programme_14_AreaAndPerimeter {
+
+    //Calculating area and perimeter of rectangle
+    public static void areaAndPerimeterOfRectangle(double height, double width) {
+        double perimeter = 2 * (height + width);
+        double area = (height * width);
+        System.out.println("The area of the rectangle is: " + area);
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+    }
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height of the rectangle: ");
+        double height = scanner.nextDouble();
+        areaAndPerimeterOfRectangle(width, height);
+        //Closing the scanner object
+        scanner.close();
+    }
+}
